3052b96f7c801650f8bf496b37753286197b7bdc
PureJD <6454jonny@gmail.com>
2024-04-06 21:06:36 +0100

    In progress of adding a light and dark mode options button. Not yet functional

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,31 @@ AOS.init({
   anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
 
 });
+
+function toggleTheme() {
+  const currentTheme = document.documentElement.getAttribute("data-theme");
+  let targetTheme = "light";
+
+  if (currentTheme === "light") {
+    targetTheme = "dark";
+  } else {
+    targetTheme = "light";
+  }
+
+  document.documentElement.setAttribute("data-theme", targetTheme);
+  localStorage.setItem("theme", targetTheme); // Save theme to localStorage
+}
+
+
+// To load the theme when the page loads
+document.addEventListener("DOMContentLoaded", () => {
+
+  const savedTheme = localStorage.getItem("theme") || "light"; // Default to light theme
+  document.documentElement.setAttribute("data-theme", savedTheme);
+
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  
+  document.getElementById("theme-toggle-button").addEventListener("click", toggleTheme);
+});
